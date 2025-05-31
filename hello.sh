11c30d755daf1b17daa21bfdb0e2b03969f493e0
Abhishek Prasad <abprsd95@gmail.com>
2025-05-31 14:40:16 +0000

    m

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Hello, World!"
+echo "This is a simple script."
+echo "It prints a greeting message."
+# End of script
+echo "Have a great day!"
+echo "Goodbye!"
+# Additional message
+echo "Thank you for using this script!"
+# This script demonstrates basic shell scripting
